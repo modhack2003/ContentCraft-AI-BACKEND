dfb45def7f49385052ddc07c12e6b3d5089c23ea
bikram@dey2003 <62688713+modhack2003@users.noreply.github.com>
2024-06-14 18:26:58 +0000

    Modify

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+const router = express.Router();
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,6 +13,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log(err));
 
 app.use('/api/auth', require('./routes/auth'));
-
+router.get('/', (req, res) =>{
+  res.send('working')
+});
+           
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
